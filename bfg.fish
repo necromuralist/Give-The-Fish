5a8dfa630b052187facbee9061234cefe453e8bc
necromuralist <3393973+necromuralist@users.noreply.github.com>
2018-10-31 11:09:06 -0700

    Updated to match current setup
     - bfg.fish: bfg version number removed from executable name
     - interfaces.fish: changed to use ``ip address`` and better grep

--- bfg.fish
+++ bfg.fish
@@ -3,7 +3,7 @@ function bfg
   cd $repository
   git gc
   cd ../  
-  java -jar $HOME/bin/bfg-1.12.16.jar --strip-blobs-bigger-than 50M $repository
+  java -jar $HOME/bin/bfg.jar --strip-blobs-bigger-than 50M $repository
   cd $repository
   git reflog expire --expire=now --all
   git gc --prune=now --aggressive
