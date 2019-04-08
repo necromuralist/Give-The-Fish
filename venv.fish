a530984a4217ec29cfbe4d3c6b5a47dd9c21db31
necromuralist <3393973+necromuralist@users.noreply.github.com>
2019-04-08 13:31:57 -0700

    Updated venv to change to .virtualenvs and back to current directory

--- venv.fish
+++ venv.fish
@@ -1,7 +1,11 @@
 function venv
+  set CURRENT_DIRECTORY (pwd)
   set ENVIRONMENT_NAME $argv[1]
+
+  cd $HOME/.virtualenvs
   python3 -m venv $ENVIRONMENT_NAME
   vf activate $ENVIRONMENT_NAME
   pip install wheel --upgrade
   pip install pip --upgrade
+  cd $CURRENT_DIRECTORY
 end
