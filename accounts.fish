e7da9d2a0bf802b4665cc9819bc6c178fdeb2399
necromuralist <3393973+necromuralist@users.noreply.github.com>
2019-05-03 12:18:23 -0700

    Moved accounts into sub-folder

--- accounts.fish
+++ accounts.fish
@@ -1,5 +1,5 @@
 function accounts
-    set path "$HOME/documents/pcloud_drive/roku_chiji/fusu/accounts.org.gpg"
+    set path "$HOME/documents/pcloud_drive/roku_chiji/fusu/accounts/accounts.org.gpg"
     echo $path
     emacs $path &
 end
