7a0e767a595e70f88483c7acba03baa6db2b8283
necromuralist <3393973+necromuralist@users.noreply.github.com>
2019-06-08 13:01:45 -0700

    Removed the hp drive from mount-drives

--- mount_drives.fish
+++ mount_drives.fish
@@ -2,9 +2,6 @@ function mount_drives
  echo "PCloud"
  cryfs ~/pCloudDrive/encrypted ~/documents/pcloud_drive
  echo
- echo "Data HP"
- cryfs ~/data/hp ~/documents/data/hp
- echo
  echo "Drop Box"
  cryfs ~/Dropbox/encrypted ~/documents/drop_box
 end
