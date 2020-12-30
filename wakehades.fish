0c5d2d4487dd3b466c060ae92a1ad94d9a7b2910
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2020-12-29 20:50:31 -0800

    changed wake on lan to require MAC address as argument

--- wakehades.fish
+++
@@ -1,4 +0,0 @@
-function wakehades
-# sudo etherwake -i wlp4s0 38:d5:47:79:ab:0b
- wakeonlan -i 192.168.86.255 38:d5:47:79:ab:0b
-end
---
+++ wakeserver.fish
@@ -0,0 +1,3 @@
+function wakeserver
+ wakeonlan -i 192.168.86.255 $argv
+end
