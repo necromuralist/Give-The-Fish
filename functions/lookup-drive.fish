da783284d7291838e6c58a2b45bcabcac9fa649c
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-05-15 17:17:29 -0500

    Added description to lookup-drive

--- functions/lookup-drive.fish
+++ functions/lookup-drive.fish
@@ -1,4 +1,4 @@
-function lookup-drive
+function lookup-drive --description "Lookup the device file for the mount-data function (e.g. sdc)"
   lsusb | grep "Western"
   set -l device (sudo fdisk -l | grep "Elements" -B 1 | grep -o '/dev\/[a-z]*' | grep -o "[a-z]*\$")
   lsblk -e7 | grep $device
