28d70bb8b0024fe5315fd8696d00cdea02ed9f16
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-06-17 18:36:56 -0500

    Added link to bug report showing how to use completions

--- completions/choose.fish
+++ completions/choose.fish
@@ -1,1 +1,2 @@
+# taken from https://github.com/fish-shell/fish-shell/issues/9285#issuecomment-1281498635
 complete -x choose -a '(complete -C "__fish_command_without_completions "(commandline --current-token --cut-at-cursor))'
