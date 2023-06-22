9fa2fc7877432b6a95970823c15d6672fc495a77
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2023-06-22 14:48:03 -0500

    Adding las function as a noweb post

--- functions/las.fish
+++ functions/las.fish
@@ -1,3 +1,8 @@
 function las
-    ls -a | less
+    ls --almost-all \
+       --color \
+       --dereference-command-line \
+       --human-readable \
+       -l $argv | \
+        less --RAW-CONTROL-CHARS
 end
