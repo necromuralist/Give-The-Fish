4f12f54c32e760838211dede98be75fbfde66630
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2024-04-02 18:31:57 -0500

    re-adding changes after failed pull

---
+++ dingehaufen/hades.fish
@@ -0,0 +1,7 @@
+set --export MOZILLA_PROFILE "$HOME/snap/firefox/common/.mozilla/firefox/mg8ry5cl.default"
+
+# wake monstery server
+set --export ATHENA "04:92:26:c3:a6:c7"
+
+# monstery LAN
+set --export APOLLO "192.168.86.172"
