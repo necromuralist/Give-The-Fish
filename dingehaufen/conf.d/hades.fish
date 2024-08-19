58e85ac6a2dfa617dc2f246d80fd2f2dc9d8c466
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2024-08-19 15:23:09 -0400

    Moved the 'choice' abbreviations to separate config file

--- dingehaufen/conf.d/hades.fish
+++ dingehaufen/conf.d/hades.fish
@@ -5,8 +5,3 @@ set --export ATHENA "04:92:26:c3:a6:c7"
 
 # monstery LAN
 set --export APOLLO "192.168.86.172"
-
-abbr --add -- word "random choice (cat ~/.local/share/choice/words.txt)"
-
-abbr --add -- motif "random choice (gzip -cd \
-    ~/.local/share/choice/motif-index.txt.gz)"
