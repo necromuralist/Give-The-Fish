eb257d425670965875d222c6cde4c10764bb6601
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2025-03-19 15:08:00 -0400

    Adding hostgrep function post

--- functions/hostgrep.fish
+++ functions/hostgrep.fish
@@ -1,3 +1,9 @@
-function hostgrep
-   cat /etc/hosts | grep $argv
+function hostgrep --argument-names HOST --description "Look for a hostname."
+
+    # check the hosts file
+    grep $HOST /etc/hosts
+
+    # check the ssh configuration
+    grep --after-context 3 $HOST $HOME/.ssh/config
+
 end
