4b71aa9a76e5c5de2b3ac27532d1d051137a5384
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2025-06-14 18:11:56 -0400

    Added vterm-printf function
     - function to help run fish with emacs' vterm (terminal emulator)
     - https://github.com/akermu/emacs-libvterm

--- dingehaufen/conf.d/hades.fish
+++ dingehaufen/conf.d/hades.fish
@@ -10,3 +10,15 @@ set --export APOLLO "192.168.86.172"
 # if status --is-interactive
 #   protonvpn-cli status
 # end
+
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
