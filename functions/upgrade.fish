3dd726df5eda29a93f813eac8fe8df7c32add582
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2025-06-24 22:05:25 -0400

    Adding post for 'upgrade' function and adding snap and flatpak upgrades

--- functions/upgrade.fish
+++ functions/upgrade.fish
@@ -1,6 +1,24 @@
-function upgrade
+function upgrade --description "Upgrade packages."
+    function boldblue
+        set_color --bold blue
+        echo $argv
+        set_color normal
+    end
+
+    boldblue Checking apt
     sudo apt-fast update --yes
-    sudo apt list --upgradeable -a
+    sudo apt-get --just-print upgrade
+
+    boldblue Upgrading apt
     sudo apt-fast full-upgrade --yes
     sudo apt-get autoremove --yes
+
+    boldblue Updating snaps
+    sudo snap refresh
+
+    boldblue Updating flatpak
+    flatpak update --assumeyes
+
+    boldblue Uninstalling unused flatpaks
+    flatpak uninstall --unused --assumeyes
 end
