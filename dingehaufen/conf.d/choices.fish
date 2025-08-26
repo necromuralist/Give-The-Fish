1908c67bc67761d8eb1d20e52458a25ed8cb9adf
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2025-08-26 15:53:18 -0400

    updated random choices and added emacs tealdeer patch

--- dingehaufen/conf.d/choices.fish
+++ dingehaufen/conf.d/choices.fish
@@ -1,4 +1,6 @@
 abbr --add -- word "random choice (cat ~/.local/share/choice/words.txt)"
 
-abbr --add -- motif "random choice (gzip -cd \
-    ~/.local/share/choice/motif-index.txt.gz)"
+abbr --add -- motif "zcat \
+    ~/.local/share/choice/motif-index.txt.gz | shuf -n1"
+
+abbr --add -- imdb "zcat /media/irvin/datasets/imdb/movie.basics.tsv.gz | shuf -n1"
---
+++ dingehaufen/tealdeer/pages/emacs.patch.md
@@ -0,0 +1,3 @@
+- Use a different initialization file when starting up:
+
+`emacs --init-directory={{path/to/directory-with-init/}}`
