6ab09a33e01fb64ee6327f5aeb49cd0e2cdba4a0
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2026-09-08 19:57:06 -0400

    Added SSH_KEY_TO_AUTOLOAD to ssh-agent function and alt-l keybinding
    - Updated ssh-agent.fish to check for environment variable instead
      of hard-coding file-name
    
    - Changed alt-l keybinding to lowercase-word instead of list directory

--- dingehaufen/conf.d/ssh-agent.fish
+++ dingehaufen/conf.d/ssh-agent.fish
@@ -1,3 +1,9 @@
-if status --is-interactive
-    keychain --eval --quiet --quick id_ed25519 | source
+if status is-login
+    and status is-interactive
+    if -n $SSH_KEYS_TO_AUTOLOAD
+	keychain --eval $SSH_KEYS_TO_AUTOLOAD | source
+    else
+	echo "SSH_KEYS_TO_AUTOLOAD environmentvariable not set"
+	echo "add key-file-name(s) to this variable"
+    end
 end
