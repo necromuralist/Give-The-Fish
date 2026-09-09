f07f05ac1cac7d1753c766220b4fbcc6cd1802c2
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2026-09-09 11:25:32 -0400

    Adding missing 'test' command to ssh-agent config

--- dingehaufen/conf.d/ssh-agent.fish
+++ dingehaufen/conf.d/ssh-agent.fish
@@ -1,6 +1,6 @@
 if status is-login
     and status is-interactive
-    if -n $SSH_KEYS_TO_AUTOLOAD
+    if test -n $SSH_KEYS_TO_AUTOLOAD
 	keychain --eval $SSH_KEYS_TO_AUTOLOAD | source
     else
 	echo "SSH_KEYS_TO_AUTOLOAD environmentvariable not set"
